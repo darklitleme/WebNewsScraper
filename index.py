da2e165a379c04da8bceb052366774b04620b48f
darklitleme <darklitleme@gmail.com>
2024-01-28 14:11:25 +1000

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,17 @@
 from WebScraper import getTokenSentiment
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-
     return "jess is stinky"
+
 @app.route("/getStockReview", methods=['GET'])
 def getStockInfo():
     stockToGet = request.args.get('stock')
     positive , negative = getTokenSentiment()
-    return jsonify({
-        "positive":positive,
-        "negative":negative
-    })
+    return (
+        "positive = " , positive,
+        "negative = " , negative)
