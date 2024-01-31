f4a498d28ecbeb56e39f835723cd3d7d3c62851e
darklitleme <darklitleme@gmail.com>
2024-01-31 10:16:25 +1000

    Update index.py

--- index.py
+++ index.py
@@ -22,4 +22,4 @@ def getStockInfo():
 @app.route("/getTokenList")
 def getTokenListReq():
     ret = getTokenList()
-    return jsonify( {"listOfTokens" : ret })
+    return jsonify( {"listOfTokens" : jsonify(ret) })
